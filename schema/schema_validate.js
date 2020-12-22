54704b73983788391dd69138c9b69aba11e60c95
Mark Howe <mvahowe@gmail.com>
2020-12-22 12:14:41 +0100

    Import schema

---
+++ schema/schema_validate.js
@@ -0,0 +1,10 @@
+const fs = require('fs-extra');
+const Validator = require('jsonschema').Validator;
+const schema = fs.readJsonSync("./proskomma_serialized_0_2.json");
+const data = fs.readJsonSync(process.argv[2]);
+const validationReport = new Validator().validate(data, schema);
+if (validationReport.errors.length > 0) {
+    console.log(JSON.stringify({valid: false, errors: validationReport.errors}, null, 2));
+} else {
+    console.log(JSON.stringify({valid: true}, null, 2));
+}
