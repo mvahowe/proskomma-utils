7b60122413ad1a66af21fb1afc972861e4fb27b6
Mark Howe <mvahowe@gmail.com>
2021-01-05 18:34:47 +0100

    Update schema; unit test schema

---
+++ test/code/serialize.js
@@ -0,0 +1,24 @@
+const test = require('tape');
+const fs = require('fs-extra')
+const path = require('path');
+const Validator = require('jsonschema').Validator;
+
+const serializedSchema = require(path.resolve(__dirname, '../../src/schema/proskomma_serialized_0_2.json'));
+
+const testGroup = "Validate Schema";
+
+test(
+    `Validate valid document (${testGroup})`,
+    async function (t) {
+        try {
+            t.plan(2);
+            const serialized = fs.readJsonSync(path.resolve(__dirname, "../test_data/serialize_example.json"));
+            t.ok(serialized);
+            const validationReport = new Validator().validate(serialized, serializedSchema);
+            console.log(validationReport.errors)
+            t.equal(validationReport.errors.length, 0);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+);
