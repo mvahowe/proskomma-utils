047b0ef1b804eae151d4e27302fea4e6c70f28f4
Mark Howe <mvahowe@gmail.com>
2021-03-23 12:02:25 +0100

    Attempting reverse versification

--- src/lib/versification.js
+++ src/lib/versification.js
@@ -8,7 +8,7 @@ const vrs2json = vrsString => {
         vrsString
             .split(/[\n\r]+/)
             .map(l => l.trim())
-            .map(l => xre.exec(l, xre("^([A-Z1-6]{3} [0-9]+:[0-9]+(-[0-9]+)) = ([A-Z1-6]{3} [0-9]+:[0-9]+(-[0-9]+))$")))
+            .map(l => xre.exec(l, xre("^([A-Z1-6]{3} [0-9]+:[0-9]+(-[0-9]+)?) = ([A-Z1-6]{3} [0-9]+:[0-9]+[a-z]?(-[0-9]+)?)$")))
         ) {
         if (!vrsLineBits) {
             continue;
@@ -18,4 +18,17 @@ const vrs2json = vrsString => {
     return {mappedVerses: ret};
 }
 
-module.exports = { vrs2json };
+const reverseVersification = vrsJson => {
+    // Doesn't attempt to handle the hypothetical situation of verse ranges intersecting with versification ranges
+    const ret = {};
+    for (const [fromSpec, toSpec] of Object.entries(vrsJson.mappedVerses)) {
+        if (toSpec in ret) {
+            ret[toSpec].push(fromSpec);
+        } else {
+            ret[toSpec] = [fromSpec];
+        }
+    }
+    return {reverseMappedVerses: ret};
+}
+
+module.exports = {vrs2json, reverseVersification};
--- test/code/versification.js
+++ test/code/versification.js
@@ -2,7 +2,7 @@ const test = require('tape');
 const fse = require('fs-extra');
 const path = require('path');
 
-const { vrs2json } = require("../../src/lib/versification");
+const { vrs2json, reverseVersification } = require("../../src/lib/versification");
 
 const testGroup = 'Versification';
 
@@ -14,7 +14,24 @@ test(
             const vrsString = fse.readFileSync(path.resolve(__dirname, '../test_data/versification.vrs')).toString();
             const vrsJson = vrs2json(vrsString);
             // console.log(JSON.stringify(vrsJson));
-            t.ok(Object.keys(vrsJson).length > 0);
+            t.ok(Object.keys(vrsJson.mappedVerses).length > 0);
+        } catch (err) {
+            console.log(err);
+        }
+    },
+);
+
+test(
+    `reverseVersification (${testGroup})`,
+    function (t) {
+        try {
+            t.plan(1);
+            const vrsString = fse.readFileSync(path.resolve(__dirname, '../test_data/versification.vrs')).toString();
+            const vrsJson = vrs2json(vrsString);
+            // console.log(JSON.stringify(vrsJson, null, 2));
+            const reversed = reverseVersification(vrsJson);
+            console.log(JSON.stringify(reversed, null, 2));
+            t.ok(Object.keys(reversed.reverseMappedVerses).length > 0);
         } catch (err) {
             console.log(err);
         }
