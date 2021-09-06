81bea81f2c7d15e0051759826f73e6726b93b942
Mark Howe <mvahowe@gmail.com>
2021-09-06 14:33:22 +0200

    Add table sequence type

--- src/lib/parser_constant_defs.js
+++ src/lib/parser_constant_defs.js
@@ -11,6 +11,7 @@ const parserConstants = {
             header: '*',
             remark: '*',
             sidebar: '*',
+            table: '*',
         },
         inlineSequenceTypes: {
             footnote: '*',
