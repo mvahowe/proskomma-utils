7efbe63b2c29b0208eeb11da91503c6dbfd1421e
Mark Howe <mvahowe@gmail.com>
2021-09-09 10:10:24 +0200

    Add tree sequence types

--- src/lib/parser_constant_defs.js
+++ src/lib/parser_constant_defs.js
@@ -12,6 +12,8 @@ const parserConstants = {
             remark: '*',
             sidebar: '*',
             table: '*',
+            tree: '*',
+            treeContent: '*',
         },
         inlineSequenceTypes: {
             footnote: '*',
