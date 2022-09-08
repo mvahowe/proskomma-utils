7c920461187b08e2120e2275ecd6de2a5ebbe4f0
DanielC-N <france.monde06@gmail.com>
2022-09-08 12:17:43 +0200

    modified 'generate_id.js' because it was still using 'uuid' instead of 'pure-uuid'

--- src/lib/generate_id.js
+++ src/lib/generate_id.js
@@ -1,7 +1,7 @@
-import { v4 } from 'uuid';
+import UUID from 'pure-uuid';
 import btoa from 'btoa';
 const generateId = () =>  {
-    return btoa(v4()).substring(0, 12);
+    return btoa(new UUID(4)).substring(0, 12);
 }
 
 export { generateId };
